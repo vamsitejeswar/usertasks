21885311aa0b242de2cd1447e1103cbbce6f3974
vamsi padmaraju <vamsitejeswar@gmail.com>
2026-01-03 13:20:56 +0530

    code

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import User, Task
+
+admin.site.register(User)
+admin.site.register(Task)
